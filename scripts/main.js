16e63885c7fb3dce93e4e929880419a295959f1d
Your Name <creighton@creightonsoukup.com>
2016-08-09 07:04:29 -0600

    flikr api impletmented

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
-//function recAreas =
-//on click
-//choose seclect region
-//choose selected types
 $(document).ready(function() {
+
     $('select').material_select();
+    $(".slider").on('focus', function() {
+        $(this).slider()
+    })
 });
 
+
 var currentRecArea = ""
+var selectedRecArea = ""
 
 
 
@@ -19,7 +21,9 @@ var activityId = {
     }
     //Gets checked values
 $("button").on('click', function(event) {
+
     event.preventDefault();
+
 
     var activities = []
     var checkbox = $("input[type='checkbox']")
@@ -49,32 +53,92 @@ $("button").on('click', function(event) {
 
 
     var recUrl = "https://ridb.recreation.gov/api/v1/recareas.json/?apikey=B368A0DCB12648F49E468F0E0391665D&state=co" + locationURL + activitiesURL
-    console.log(recUrl)
     $.get(recUrl)
         .then(function(data) {
+            $('.removecards').remove()
             makeCards(data)
         }).then(function(cards) {
 
             $("html,body").animate({
                 scrollTop: $(".cards").offset().top
             }, 2000);
+            $("form")[0].reset()
+            $(".more-info").on('click', (function() {
 
-            $(".more-info").click(function() {
                 var directions = $(this).prev()
                 directions.toggle()
-                return $(this).
+                selectedRecArea = $(this).attr("id")
+                selectedRecAreaUrl = "&tags=" + selectedRecArea.split(" ").join("+")
 
-            })
-            .then (function(data) {
-                $.get("")
-            }
+
+                photoURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3ffd3415340ed60a1dcd3548c3790329&format=json&nojsoncallback=1" + selectedRecAreaUrl
+                $.get(photoURL)
+                    .then(function photoCarousel(data) {
+
+                        var photos = []
+                        for (var i = 0; i < 10; i++) {
+                            photos.push({
+                                farmid: data["photos"]["photo"][i]["farm"],
+                                serverid: data["photos"]["photo"][i]["server"],
+                                photoid: data["photos"]["photo"][i]["id"],
+                                secret: data["photos"]["photo"][i]["secret"]
+                            })
+                        }
+                        var img1 = 'https://farm' + photos[0].farmid + '.staticflickr.com/' + photos[0].serverid + '/' + photos[0].photoid + '_' + photos[0].secret + '.jpg'
+                        var img2 = 'https://farm' + photos[1].farmid + '.staticflickr.com/' + photos[1].serverid + '/' + photos[1].photoid + '_' + photos[1].secret + '.jpg'
+                        var img3 = 'https://farm' + photos[2].farmid + '.staticflickr.com/' + photos[2].serverid + '/' + photos[2].photoid + '_' + photos[2].secret + '.jpg'
+                        var img4 = 'https://farm' + photos[3].farmid + '.staticflickr.com/' + photos[3].serverid + '/' + photos[3].photoid + '_' + photos[3].secret + '.jpg'
+                        var img5 = 'https://farm' + photos[4].farmid + '.staticflickr.com/' + photos[4].serverid + '/' + photos[4].photoid + '_' + photos[4].secret + '.jpg'
+                        var img6 = 'https://farm' + photos[5].farmid + '.staticflickr.com/' + photos[5].serverid + '/' + photos[5].photoid + '_' + photos[5].secret + '.jpg'
+                        var img7 = 'https://farm' + photos[6].farmid + '.staticflickr.com/' + photos[6].serverid + '/' + photos[6].photoid + '_' + photos[6].secret + '.jpg'
+                        var img8 = 'https://farm' + photos[7].farmid + '.staticflickr.com/' + photos[7].serverid + '/' + photos[7].photoid + '_' + photos[7].secret + '.jpg'
+                        var img9 = 'https://farm' + photos[8].farmid + '.staticflickr.com/' + photos[8].serverid + '/' + photos[8].photoid + '_' + photos[8].secret + '.jpg'
+                        var img10 = 'https://farm' + photos[9].farmid + '.staticflickr.com/' + photos[9].serverid + '/' + photos[9].photoid + '_' + photos[9].secret + '.jpg'
+                        $(".photo").append($("<div/>", {
+                            "class": "slider"
+                        }).append(
+                            $("<ul/>", {
+                                "class": "slides"
+                            }).append(
+                                $("<li/>").append(
+                                    $("<img/>", {
+                                        "src": "" + img1 + ""
+                                    })
+                                )
+
+                            ).append(
+                                $("<li/>").append(
+                                    $("<img/>", {
+                                        "src": "" + img2 + ""
+                                    })
+                                )
+                            ).append(
+                                $("<li/>").append(
+                                    $("<img/>", {
+                                        "src": "" + img3 + ""
+                                    })
+                                )
+                            ).append(
+                                $("<li/>").append(
+                                    $("<img/>", {
+                                        "src": "" + img4 + ""
+                                    })
+                                )
+                            )))
+
+
+                    })
+
+            }))
         })
-        .catch(function(err) {
-            console.error(err)
-        })
+
+    .catch(function(err) {
+        console.log(error)
+    })
 
 
 })
+
 
 function makeCards(data) {
 
@@ -85,10 +149,10 @@ function makeCards(data) {
         }
         $(".cards").append(
             $("<div/>", {
-                'class': 'row'
+                'class': 'row removecards',
             }).append(
                 $("<div/>", {
-                    'class': 'col s6 offset-s3'
+                    'class': 'col s8 offset-s2'
                 }).append(
                     $("<div/>", {
                         'class': 'card blue-grey darken-1'
@@ -114,7 +178,15 @@ function makeCards(data) {
                             }),
                             $("<p/>", {
                                 'text': "" + data.RECDATA[i].RecAreaDirections
+                            }),
+                            $("<span/>", {
+                                "class": "card-title amber-text",
+                                "text": 'Photos'
+                            }),
+                            $("<div/>", {
+                                "class": "photo"
                             })
+
                         ).hide()
                     ).append(
                         $("<div/>", {
@@ -122,7 +194,9 @@ function makeCards(data) {
                         }).append(
                             $("<button/>", {
                                 'class': 'more-info btn waves-effect waves-light',
-                                'text': 'More Info'
+                                'text': 'More Info',
+                                "id": "" + data.RECDATA[i].RecAreaName + ""
+
                             })
                         )
                     )
@@ -131,14 +205,3 @@ function makeCards(data) {
         )
     }
 }
-
-function flikrUrl () {
-
-}
-
-//  $("#card").on('click', function(event) {
-//    $(this).toggleClass("active")
-//
-//  }).then function (data)
-//
-// console.log(data.RECDATA[1].RecAreaName)
---
+++ scripts/photos.js
@@ -0,0 +1,17 @@
+var latAndLong = [{
+    region: "fort collins",
+    latitude: 40.5853,
+    longitude: -105.0844
+}, {
+    region: "boulder",
+    latitude: 40.0150,
+    longitude: -105.2705
+}, {
+    region: "denver metro",
+    latitude: 39.7392,
+    longitude: -104.9903
+}, {
+    region: "colorado springs",
+    latitude: 38.8339,
+    longitude: -104.8214
+}]
